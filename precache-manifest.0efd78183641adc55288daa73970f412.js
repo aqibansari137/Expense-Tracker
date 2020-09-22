a979d927bad3dd339a65495cbe9e1f1aff6ef87b
aqib ansari <aqibansari137@gmail.com>
2020-09-22 13:27:03 +0530

    Updates

---
+++ precache-manifest.0efd78183641adc55288daa73970f412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1809a25189b213202e8c956992bc169",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "06c1d5338cd923a8dd65",
+    "url": "/Expense-Tracker/static/css/main.8a50243e.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/Expense-Tracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Tracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c1d5338cd923a8dd65",
+    "url": "/Expense-Tracker/static/js/main.a43068b8.chunk.js"
+  },
+  {
+    "revision": "8b85743ef164798ac742",
+    "url": "/Expense-Tracker/static/js/runtime-main.185c89e8.js"
+  }
+]);
